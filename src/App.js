48bd8ede6ac11ac2e00104c988ace63ad6165de1
Raphael (depuis son fixe) <raphael.ricard@epitech.eu>
2017-09-30 21:11:35 +0200

    removed some spaces

--- src/App.js
+++ src/App.js
@@ -5,13 +5,11 @@ class App extends Component {
         return (
             <div className="App">
                 <header className="App-header">
-                    <h1 className="App-title"> Welcome to React </h1> {" "}
-                </header>{" "}
-                {" "}
+                    <h1 className="App-title"> Welcome to React </h1>
+                </header>
                 <p className="App-intro">
-                    To get started, edit <code> src / App.js </code> and save to reload. {" "}
-                </p>{" "}
-                {" "}
+                    To get started, edit <code> src / App.js </code> and save to reload.
+                </p>
             </div>
         );
     }
