f79b010282c9a352b690106ec53dc9984315047c
Telokis <telokis0@gmail.com>
2017-11-08 00:25:44 +0100

    Header is now responsive

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,8 @@ import { Link } from "react-router-dom";
 import { Grid } from "semantic-ui-react";
 import PropTypes from "prop-types";
 import { withRouter } from "react-router";
-import { Menu, Container, Button, Icon } from "semantic-ui-react";
+import { Icon } from "semantic-ui-react";
+import { Label, Nav, NavItem, Navbar, NavDropdown, MenuItem } from "react-bootstrap";
 import { logoutAttempt } from "../actions/creators/login";
 
 class Header extends Component {
@@ -13,54 +14,61 @@ class Header extends Component {
   adminLevel = (lvl, comp) =>
     this.props.isAuthenticated && this.props.userInfos.adminLevel >= lvl ? comp : null;
 
+  stuff = pathname => (
+    <Navbar.Collapse>
+      <Nav>
+        {this.guest(
+          <NavItem active={pathname === "/login"} href="/login">
+            Connexion
+          </NavItem>,
+        )}
+        {this.auth(
+          <NavItem active={pathname === "/"} href="/">
+            Accueil
+          </NavItem>,
+        )}
+
+        {this.adminLevel(
+          1,
+          <NavItem active={pathname === "/items"} href="/items">
+            Objets
+          </NavItem>,
+        )}
+
+        {this.adminLevel(
+          2,
+          <NavItem active={pathname === "/createAPIKey"} href="/createAPIKey">
+            Token API
+          </NavItem>,
+        )}
+
+        {this.adminLevel(
+          3,
+          <NavItem active={pathname === "/invite"} href="/invite">
+            Créer une invitation
+          </NavItem>,
+        )}
+      </Nav>
+      <Nav pullRight>
+        {this.auth(
+          <NavItem onClick={this.props.onLogout}>
+            <Label bsStyle="danger">
+              <Icon name="power" />Déconnexion
+            </Label>
+          </NavItem>,
+        )}
+      </Nav>
+    </Navbar.Collapse>
+  );
+
   render() {
     const { pathname } = this.props.location;
 
     return (
-      <Container>
-        <Menu pointing size="large">
-          {this.guest(
-            <Menu.Item active={pathname === "/login"} href="/login">
-              Connexion
-            </Menu.Item>,
-          )}
-
-          {this.auth(
-            <Menu.Item active={pathname === "/"} href="/">
-              Accueil
-            </Menu.Item>,
-          )}
-
-          {this.adminLevel(
-            1,
-            <Menu.Item active={pathname === "/items"} href="/items">
-              Objets
-            </Menu.Item>,
-          )}
-
-          {this.adminLevel(
-            2,
-            <Menu.Item active={pathname === "/createAPIKey"} href="/createAPIKey">
-              Token API
-            </Menu.Item>,
-          )}
-
-          {this.adminLevel(
-            3,
-            <Menu.Item active={pathname === "/invite"} href="/invite">
-              Créer une invitation
-            </Menu.Item>,
-          )}
-
-          <Menu.Item position="right">
-            {this.auth(
-              <Button color="red" onClick={this.props.onLogout}>
-                <Icon name="power" />Déconnexion
-              </Button>,
-            )}
-          </Menu.Item>
-        </Menu>
-      </Container>
+      <Navbar fluid>
+        <Navbar.Toggle />
+        {this.stuff(pathname)}
+      </Navbar>
     );
   }
 }
